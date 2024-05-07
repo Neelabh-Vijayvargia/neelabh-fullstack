9454bff51e2d22b19a5610255f2c360726bf34b0
Neelabh Vijayvargia <neelabh.vijayvargia@gmail.com>
2024-05-06 23:49:15 -0500

    backend, roughdraft

---
+++ backend/models.py
@@ -0,0 +1,18 @@
+## contains database models
+## how to interact with databse
+
+from config import db
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(80), unique = False, nullable = False)
+    last_name = db.Column(db.String(80), unique = False, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+
+    def to_json(self):
+        return {
+            "id" : self.id,
+            "firstName" : self.first_name,
+            "lastName" : self.last_name,
+            "email" : self.email
+        }
